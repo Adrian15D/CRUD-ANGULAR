0aec08ac8801dcdbf3f4f574aca925bffc926c54
Adrian15D <adrian15dn2002@gmail.com>
2023-04-18 23:10:33 -0600

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Employee} from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CRUD';
+
+  EmployeeArray: Employee[] = [
+    {id: 1, name: "Adrian", country: "MEX"},
+    {id: 2, name: "Ximena", country: "EUA"},
+    {id: 3, name: "Abdul", country: "IND"}
+  ];
+
+  selectedEmployee: Employee = {id:0, name: '', country: ''};
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+    this.selectedEmployee.id = this.EmployeeArray.length + 1;
+    this.EmployeeArray.push(this.selectedEmployee);
+    }
+
+
+
+    this.selectedEmployee = new Employee(0,'','');
+
+  }
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+
+
+  } 
+
+  delete(){
+
+    if(confirm('Estas seguro de querer eliminar este dato?')){
+    this.EmployeeArray = this.EmployeeArray.filter(x => x != this.selectedEmployee);
+    this.selectedEmployee = new Employee(0,'','');
+    }
+  }
+
 }
---
+++ src/app/models/employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    constructor(
+   public id: number = 0,
+   public name: string,
+   public country: string
+    ){}
+}
+
+   
+
